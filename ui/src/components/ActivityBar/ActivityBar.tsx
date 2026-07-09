c2c2377003027968d69a76dbc828c833d2d8cb3d
nidhishmanimaran-rgb <nidhishmanimaran@gmail.com>
2026-07-09 12:01:36 +0530

    v0.1.0 Project architecture

--- ui/src/components/ActivityBar/ActivityBar.tsx
+++ ui/src/components/ActivityBar/ActivityBar.tsx
@@ -1,16 +1,48 @@
 import "./ActivityBar.css";
 
+import {
+    VscHome,
+    VscFiles,
+    VscGraph,
+    VscRobot,
+    VscDatabase,
+    VscSettingsGear
+} from "react-icons/vsc";
+
 function ActivityBar() {
+
     return (
+
         <div className="activity-bar">
-            <div className="activity-icon">🏠</div>
-            <div className="activity-icon">📁</div>
-            <div className="activity-icon">📊</div>
-            <div className="activity-icon">🤖</div>
-            <div className="activity-icon">🗄️</div>
-            <div className="activity-icon">⚙️</div>
+
+            <div className="activity-icon">
+                <VscHome size={24}/>
+            </div>
+
+            <div className="activity-icon">
+                <VscFiles size={24}/>
+            </div>
+
+            <div className="activity-icon">
+                <VscGraph size={24}/>
+            </div>
+
+            <div className="activity-icon">
+                <VscRobot size={24}/>
+            </div>
+
+            <div className="activity-icon">
+                <VscDatabase size={24}/>
+            </div>
+
+            <div className="activity-icon">
+                <VscSettingsGear size={24}/>
+            </div>
+
         </div>
+
     );
+
 }
 
 export default ActivityBar;
